7a0d0f488644848b44753445d3dd981bbc2a94f6
Clint Plozay <ctwilb88@gmail.com>
2023-11-19 17:41:03 -0600

    added var

--- television.py
+++ television.py
@@ -8,3 +8,36 @@ class Television():
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
+    def power(self):
+        if self.__status:
+            self.__status = False
+        else:
+            self.__status = True
+    def mute(self):
+        if self.__status:
+            if self.__muted:
+                self.__muted = False
+            else:
+                self.__muted = True
+    def channel_up(self):
+        if self.__status:
+            if self.__channel == Television.MAX_CHANNEL:
+                self.__channel = Television.MIN_CHANNEL
+            else:
+                self.__channel += 1
+    def channel_down(self):
+        if self.__status:
+            if self.__channel == Television.MIN_CHANNEL:
+                self.__channel = Television.MAX_CHANNEL
+            else:
+                self.__channel -= 1
+    def volume_up(self):
+        self.__muted = False
+        if self.__volume != Television.MAX_VOLUME:
+            self.__volume += 1
+    def volume_down(self):
+        self.__muted = False
+        if self.__volume != Television.MIN_VOLUME:
+            self.__volume -= 1
+    def __str__(self):
+        return f"Power - {self.__status}, Channel - {self.__channel}, Volume - {self.__volume}"
